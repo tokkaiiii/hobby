629ec6a8905c9c018e3044acbb19f2afc7d1ed41
heosangbeom <luv_3_u@naver.com>
2025-03-03 21:54:50 +0900

    feat remove dummydata

--- lib/data.ts
+++ lib/data.ts
@@ -1,4 +1,3 @@
-import { Post } from "@/types/domain"
 
 export const categories = [
     {
@@ -6,16 +5,4 @@ export const categories = [
         src: "/img/uniqlo01.jpg",
     },
     
-]
-
-export const posts: Post[] = [
-    {
-        id: 1,
-        title: "우연히 구매한 티셔츠",
-        subtitle: "고양이가 귀여워서 구매했습니다.",
-        thumbnail: "/img/uniqlo01.jpg",
-        category: "uniqlo",
-        createdAt: "2025-03-03",
-        updatedAt: "2025-03-03",
-    }
 ]
