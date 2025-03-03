6ff5c93f3f376862db8b9e7444628b0b0decda94
heosangbeom <luv_3_u@naver.com>
2025-03-03 21:52:32 +0900

    feat 프라이빗 변수로 변경

--- api/storage.ts
+++ api/storage.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.PRIVATE_SUPABASE_URL;
+const supabaseAnonKey = process.env.PRIVATE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error("Missing Supabase environment variables");
@@ -10,17 +10,17 @@ if (!supabaseUrl || !supabaseAnonKey) {
 const supabaseStorage = createClient(supabaseUrl, supabaseAnonKey);
 
 export const getImageUrl = (path: string) => {
-    const { data } = supabaseStorage.storage.from(process.env.NEXT_PUBLIC_STORAGE_BUCKET!).getPublicUrl(path);
+    const { data } = supabaseStorage.storage.from(process.env.PRIVATE_SUPABASE_STORAGE_BUCKET!).getPublicUrl(path);
     return data.publicUrl;
 }
 
 export const uploadImage = async (path: string, file: File) => {
-    const { data, error } = await supabaseStorage.storage.from(process.env.NEXT_PUBLIC_STORAGE_BUCKET!).upload(path, file);
+    const { data, error } = await supabaseStorage.storage.from(process.env.PRIVATE_SUPABASE_STORAGE_BUCKET!).upload(path, file);
     return data;
 }
 
 export const deleteImage = async (path: string) => {
-    const { data, error } = await supabaseStorage.storage.from(process.env.NEXT_PUBLIC_STORAGE_BUCKET!).remove([path]);
+    const { data, error } = await supabaseStorage.storage.from(process.env.PRIVATE_SUPABASE_STORAGE_BUCKET!).remove([path]);
     return data;
 }
 
