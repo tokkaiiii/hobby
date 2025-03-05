a21efa6d3f3aeb978618f8eb550e5ddeabc1c28d
heosangbeom <luv_3_u@naver.com>
2025-03-06 08:20:17 +0900

    feat error props 에 값 전달하지 않아서 발생한 에러 수정

--- app/post/[id]/error.tsx
+++ app/post/[id]/error.tsx
@@ -2,8 +2,8 @@
 
 import ErrorMessage from "@/components/ErrorMessage"
 
-export default function Error() {
+export default function Error({ error, reset }: { error: Error, reset: () => void }) {
     return (
-        <ErrorMessage />
+        <ErrorMessage error={error} reset={reset} />
     )
 }
